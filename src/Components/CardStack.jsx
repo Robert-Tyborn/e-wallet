a8c2786d65e0a6b9aab789f53e65a25482f46256
Robert-Tyborn <144322797+Robert-Tyborn@users.noreply.github.com>
2024-04-24 18:29:24 +0200

    active card

--- src/Components/CardStack.jsx
+++ src/Components/CardStack.jsx
@@ -3,16 +3,27 @@ import { useSelector } from "react-redux";
 import Card from "./Card";
 import "../Styles/CardStack.css";
 
-function CardStack() {
+function CardStack({ activeCard, onCardClick }) {
   const cards = useSelector((state) => state.card);
 
   return (
     <div className="card-stack-container">
-      {Array.isArray(cards) && cards.map((card, index) => (
-        <div key={index} className="card-stack-item" style={{ marginTop: index * 3 + "rem" }}>
-          <Card formData={card} />
-        </div>
-      ))}
+      {Array.isArray(cards) &&
+        cards.map((card, index) => {
+          if (card === activeCard) {
+            return null; 
+          }
+          return (
+            <div
+              key={index}
+              className="card-stack-item"
+              style={{ marginTop: index * 3 + "rem" }}
+              onClick={() => onCardClick(card)}
+            >
+              <Card formData={card} />
+            </div>
+          );
+        })}
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "../Components/Card";
 import "../Styles/Home.css";
 import CardStack from "../Components/CardStack";
 
 function Home() {
+  const [activeCard, setActiveCard] = useState(null);
+
   const formData = {
     cardNumber: "XXXX XXXX XXXX XXXX",
     cardholderName: "FIRSTNAME LASTNAME",
@@ -13,17 +15,25 @@ function Home() {
     vendor: "",
   };
 
+  const handleCardClick = (card) => {
+    setActiveCard(card);
+  };
+
   return (
     <div>
       <div className="top">
         <h1 className="home-title">E-WALLET</h1>
       </div>
-      <Card formData={formData} />
-      <CardStack />
+      {activeCard ? (
+        <Card formData={activeCard} />
+      ) : (
+        <Card formData={formData} />
+      )}
+      <CardStack activeCard={activeCard} onCardClick={handleCardClick} />
       <div className="button-container">
-      <Link to="/addcard" className="new-card-btn">
-        ADD A NEW CARD
-      </Link>
+        <Link to="/addcard" className="new-card-btn">
+          ADD A NEW CARD
+        </Link>
       </div>
     </div>
   );
